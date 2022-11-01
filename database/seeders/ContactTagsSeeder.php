f1260bb6036c7e40eb2c6b13ae0befeea0b9ed4c
Lendi24 <Lendic24@hotmail.com>
2022-11-01 14:56:23 +0100

    contact seeds

--- database/seeders/ContactTagsSeeder.php
+++ database/seeders/ContactTagsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ContactTag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,48 @@ class ContactTagsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $contactTags = [
+            [
+                'name'   => 'E-mail',
+                'type'   => 'text',
+                'icon'   => 'mdi-email',
+            ],
+
+            [
+                'name'   => 'Phone',
+                'type'   => 'text',
+                'icon'   => 'mdi-phone',
+            ],
+
+            [
+                'name'   => 'Discord',
+                'type'   => 'text',
+                'icon'   => 'mdi-discord',
+            ],
+
+
+
+            [
+                'name'   => 'LinkedIn',
+                'type'   => 'button',
+                'icon'   => 'mdi-linkedin',
+            ],
+
+            [
+                'name'   => 'Twitter',
+                'type'   => 'button',
+                'icon'   => 'mdi-twitter',
+            ],
+
+            [
+                'name'   => 'GitHub',
+                'type'   => 'button',
+                'icon'   => 'mdi-github',
+            ],
+        ];
+        
+        foreach ($contactTags as $key => $value) {
+            ContactTag::create($value);
+        }
     }
 }
